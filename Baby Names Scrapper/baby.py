93ae67fe2cc8ac2c13d4deec7a06e12b3223cf13
Sreeram Aj <sreeramzeno@gmail.com>
2022-02-02 00:44:41 +0530

    Update baby.py

--- Baby Names Scrapper/baby.py
+++ Baby Names Scrapper/baby.py
@@ -9,21 +9,13 @@ url = F"https://parenting.firstcry.com/baby-names/starting-with/{search_name}/"
 
 res = requests.get(url)
 soup = bs(res.text,"lxml")
-names = soup.find_all("div",{"class":"baby-name M15_42"})
-# meanings = soup.find_all("span",{"class":"nm-ming R12_9e"})
+anchor1 = soup.find_all("div",{"class":"baby-name M15_42"})
+anchor2 = soup.find_all("span",{"class":"nm-ming R12_9e"})
 with open(search_name +".txt", "a") as file:
- for div in names:
-     Names = div.text
-  
 
-     file.write(f"{Names}")
-     file.write("\n")
-     print(f"{Names}")
-
-
-    
-
-
-
-
-
+ for i,(name,meaning) in enumerate(zip(anchor1,anchor2)):
+    name = name.text.strip()
+    meaning = meaning.text.strip() 
+    file.write(f"{name}--{meaning}")
+    file.write("\n") 
+    print(f"{name}--{meaning}")
