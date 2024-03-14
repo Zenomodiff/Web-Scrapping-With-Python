348f4aaa935d8ec9c3fd5d08a599b922b486748f
Sreeram Aj <sreeramzeno@gmail.com>
2024-03-14 14:34:36 +0530

    Add files via upload

--- Unique Baby Names/data.py
+++ Unique Baby Names/data.py
@@ -1,21 +1,19 @@
-from urllib import response
 import requests
 from bs4 import BeautifulSoup as bs
+import time
 
-for i in range(1,11):
-    page_number = (i)
+for page_number in range(1, 11):
     url = f"https://nameberry.com/popular-names/unique/{page_number}"
     req = requests.get(url)
-    soup= bs(req.content,"lxml") 
-    data1 = soup.find("div",{"class":"w100pct"})
-    for i in data1:
-        j = i.find_all("a")
-        for k in j:
-            baby_name = k.find_all("span",{"class":"jsx-2400428389"}, limit=2)[-1].text.strip()
-            print(baby_name)
-            with open("data.txt","a", encoding="utf-8")as f:
-                f.write(f"{baby_name}")
-                f.write("\n")
+    soup = bs(req.content, "lxml")
+    data1 = soup.find("div", {"class": "w100pct"})
 
-
-
+    if data1:
+        j = data1.find_all("a")
+        for index, k in enumerate(j, start=1):
+            baby_name = k.find_all("span", {"class": "jsx-2400428389"}, limit=2)[-1].text.strip()
+            gender = "girl" if index % 2 == 0 else "boy"
+            print(f"{baby_name} is a {gender} name")
+            with open("data.txt","a")as f:
+               f.write(f"{baby_name} is a {gender} name")
+               f.write("\n")
